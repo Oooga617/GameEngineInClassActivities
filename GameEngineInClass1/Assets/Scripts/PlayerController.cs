0b56c65f7bec44ad866f13c4ec84ecf394331a1e
Oooga617 <johnphilip.underwood@ontariotechu.net>
2026-09-10 19:40:49 -0400

    project

---
+++ GameEngineInClass1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float moveX;
+    public float speed = 10.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
